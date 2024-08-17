c412b66ca7ca1a8a130993dd1cabfe0f657c865b
Nur Hidayatullah <nurhidayatullah.xviii@gmail.com>
2024-08-17 14:13:16 +0700

    Buat Database & Register User

--- website/models.py
+++ website/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=25)
+    desa = models.CharField(max_length=20)
+    jabatan = models.CharField(max_length=20)
+    kelahiran = models.DateField()
+    pendidikan = models.CharField(max_length=50)
+    nomor_sk = models.CharField(max_length=50)
+    tanggal_sk = models.DateField()
+    NIK = models.BigIntegerField()  
+    KK = models.BigIntegerField()
+    hp = models.CharField(max_length=15, default='N/A')
+    keterangan = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- website/views.py
+++ website/views.py
@@ -1,27 +1,49 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all() 
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST.get('username')
+        password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, "Anda Sudah Log In")
+            messages.success(request, "Anda sudah login")
             return redirect('home')
         else:
-            messages.success(request, "Coba Log In lagi")
+            messages.error(request, "Username atau password salah, coba lagi")
             return redirect('home')
-    else:
-        return render(request, 'home.html', {})
+    return render(request, 'home.html', {'records':records})
 
 def login_user(request):
+    # Implementation for login_user, if needed
     pass
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "Anda berhasil logout")
+    return redirect('home')
 
-
+def register_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Anda berhasil mendaftar")
+                return redirect('home')
+        else:
+            messages.error(request, "Ada kesalahan dalam formulir. Silakan coba lagi.")
+    else:
+        form = SignUpForm()
+    
+    return render(request, 'register.html', {'form': form})
